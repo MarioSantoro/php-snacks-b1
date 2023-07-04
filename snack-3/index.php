9a8f206451ddf635d582894557ea151a0c247264
MarioSantoro <mariosantoro124@gmail.com>
2023-07-04 12:28:22 +0200

    fix the code

--- snack-3/index.php
+++ snack-3/index.php
@@ -3,13 +3,12 @@ $array1 = ['a', 'b', 'c', 'd', 'e'];
 $array2 = ['1', '2', '3', '4', '5'];
 $array4 = [];
 
-function Arrays($array1, $array2, $array4)
+function Arrays($array1, $array2)
 {
     for ($i = 0; $i < 5; $i++) {
-        $array3 = $array1[$i] . $array2[$i];
-        $array4[] = $array3;
+        $array3[] = $array1[$i] . $array2[$i];
     }
-    var_dump($array4);
+    var_dump($array3);
 }
 
-Arrays($array1, $array2, $array4);
+Arrays($array1, $array2);
